1ea7aea1978583cab48de91773765fc76693e321
Evan Kiefl <kiefl.evan@gmail.com>
2019-10-01 17:00:32 -0500

    add helpful msg

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='clusterize',
-    version="0.2",
+    version="0.2.1",
     packages=find_packages(),
     author_email = 'kiefl.evan@gmail.com',
     author = 'Evan Kiefl',
