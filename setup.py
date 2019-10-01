b5a37fc7d6f4f88ac02781c664d378cbf28425dd
Evan Kiefl <kiefl.evan@gmail.com>
2019-10-01 17:15:43 -0500

    clean

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='clusterize',
-    version="0.2.1",
+    version="0.2.2",
     packages=find_packages(),
     author_email = 'kiefl.evan@gmail.com',
     author = 'Evan Kiefl',
